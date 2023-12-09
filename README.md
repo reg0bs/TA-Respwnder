f79266d1630ab6b6a34609d0c04201153ec39265
reg0bs <77054638+reg0bs@users.noreply.github.com>
2023-12-09 09:52:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TA-Respwnder
+Splunk App to detect responder like attacks
+
+This app can be deployed to Universal Forwarders to create a distributed detection network against LLMNR poisoning. 
+The script has mainly 2 functions:
+  - Broadcast LLMNR requests for names of services that don't exist. These can be generated randomly or manually set by you
+  - Optionally if requests are responded to it's possible to authenticate against the attacker machine. This can be used to either give the attacker some busy work or you can later on track where they used the creds to login and therefore tracking the attacker within your network.
+
+Check out `inputs.conf` in the `default` folder to find examples of input configurations.
+The script supports the following parameters:
+
+`-Names` = A list of names to query
+`-Authenticate` = Enables additional authentication if responses were received
+`-Username` = Username to use for authentication
+`-Pass` = Password to use for authentication
---
+++ bin/Respwnder.ps1
@@ -0,0 +1,74 @@
+# Author: @reg0bs
+# https://github.com/reg0bs/Find-Responder
+# Credits go to the original author of asker.py: Erik V (https://github.com/eavalenzuela)
+
+# This script checks if responses to arbitrary LLMNR requests are sent to detect responder like attacks
+
+Param (
+    [Parameter(Mandatory=$False)]
+    [String[]]
+    $Names,
+
+    [Parameter(Mandatory=$False)]
+    [Boolean]
+    $Authenticate = $False,
+
+    [Parameter(Mandatory=$False)]
+    [String]
+    $Username = 'Administrator',
+
+    [Parameter(Mandatory=$False)]
+    [String]
+    $Pass = -join (1..8 | ForEach-Object {[char]((97..122) + (48..57) | Get-Random)})
+)
+
+$ErrorActionPreference = 'Continue'
+
+function Write-Log {
+    Param (
+        [Parameter(Mandatory=$True)]
+        [Int]
+        $EventId,
+        
+        [Parameter(Mandatory=$True)]
+        [String]
+        $EventMessage
+    )
+    $Timestamp = (Get-Date).toString('yyyy/MM/dd HH:mm:ss')
+    $LogMessage = $Timestamp + ' event_id=' + $EventId + ' event_message=' + $EventMessage
+    Write-Output $LogMessage
+}
+
+function Send-Hash {
+    Param (
+        [Parameter(Mandatory=$True)]
+        [String]
+        $IpAddress
+    )
+    Write-Verbose 'Sending hashes...'
+    $RemotePath = '\\' + $IpAddress + '\c$'
+    New-SmbMapping -RemotePath $RemotePath -UserName $Username -Password $Pass -ErrorAction Ignore
+    Write-Log -EventId 2 -EventMessage "sent hashes to possible responder user=$Username"
+}
+
+function Find-Responder {
+    Write-Verbose "Starting Respwnder..."
+    If (!$Names) {
+        $Names = -join (1..12 | ForEach-Object {[char]((97..122) + (48..57) | Get-Random)})
+    }
+    # Use specified names to check for responder
+    foreach ($Name in $Names) {
+        Write-Verbose "Resolving $Name..."
+        $Responses = Resolve-DnsName -LlmnrOnly -Name $Name -ErrorAction Ignore
+        foreach ($Response in $Responses) {
+            Write-Verbose 'Found possible responder...'
+            $IpAddress = $Response.IPAddress
+            Write-Log -EventId 1 -EventMessage "`"possible responder detected`" query=$Name answer=$IpAddress"
+            If ($Authenticate) {
+                Send-Hash -IPAddress $Response.IPAddress
+            }
+        }
+    }
+}
+
+Find-Responder
